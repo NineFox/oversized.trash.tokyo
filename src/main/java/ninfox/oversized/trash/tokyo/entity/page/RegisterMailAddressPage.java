4aa1b5922613309a0f95cc771ab298751bae7fb3
Komatsubara <express.yukiguni.583@gmail.com>
2020-12-17 22:34:29 +0900

    メールアドレス登録画面の操作クラスを追加

---
+++ src/main/java/ninfox/oversized/trash/tokyo/entity/page/RegisterMailAddressPage.java
@@ -0,0 +1,40 @@
+package ninfox.oversized.trash.tokyo.entity.page;
+
+import org.openqa.selenium.support.FindBy;
+
+import com.codeborne.selenide.SelenideElement;
+
+import lombok.Getter;
+
+/**
+ * メールアドレス登録
+ * @author Komatsubara Takeshi
+ *
+ */
+@Getter
+public class RegisterMailAddressPage {
+
+    /**
+     * メールアドレス
+     */
+    @FindBy(id = "txtMail")
+    private SelenideElement mail;
+
+    /**
+     * メールアドレス(確認用)
+     */
+    @FindBy(id = "txtMail2")
+    private SelenideElement confileMail;
+
+    /**
+     * 同意チェック
+     */
+    @FindBy(id = "chkAuth")
+    private SelenideElement checkAuth;
+
+    /**
+     * メールアドレス登録
+     */
+    @FindBy(className = "btn")
+    private SelenideElement registerMail;
+}
